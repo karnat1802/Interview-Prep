a1619f3fc96967868799536b239fdee67c6ff0f3
karnat1802 <karthiknatarajan2009@gmail.com>
2018-06-26 00:59:59 -0700

    Add files via upload

--- Leet Code Practice/ParenthesisMatching.java
+++ Leet Code Practice/ParenthesisMatching.java
@@ -3,19 +3,22 @@ import java.util.*;
 public class ParenthesisMatching
 {
    public boolean isValid(String s) {
-     Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<Character>();
      for(char c : s.toCharArray())
      {
-         if(c=='{')
-            stack.push('}');
-         else if(c=='[')
-            stack.push(']');
-         else if(c=='(')
-            stack.push(')');
-        else if(stack.isEmpty() || stack.pop()!=c)
+         if(c=='{' || c=='(' || c=='[')
+             stack.push(c);
+         else if(stack.isEmpty() || (c==')' && stack.peek()!='('  ))
             return false;
+         else if(stack.isEmpty() || (c=='}' &&  stack.peek()!='{'))
+            return false;
+         else if(stack.isEmpty() || (c==']' &&  stack.peek()!='['))
+            return false;
+         else 
+             stack.pop();
      }
      return stack.isEmpty();
     }
-   }
+    }
+
 

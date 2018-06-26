0954fc6251f181ceade49f29b191748b46edbbab
karnat1802 <karthiknatarajan2009@gmail.com>
2018-06-26 01:51:03 -0700

    Add files via upload

---
+++ Leet Code Practice/strStr.java
@@ -0,0 +1,18 @@
+public class strStr
+{
+   public int strStr(String haystack, String needle) {
+        if(needle.isEmpty())
+            return 0;
+        for(int i = 0; i<=haystack.length()- needle.length();i++)
+        {
+            for(int j = 0; j<needle.length() ;j++)
+            {
+                if( haystack.charAt(i+j)!=needle.charAt(j))
+                    break;
+                if(j==needle.length()-1)
+                    return i;
+            }
+        }
+        return -1;
+    }
+}

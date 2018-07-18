8393eeae49b659fc2c15cc5e3e85d49ff97edaaf
karnat1802 <karthiknatarajan2009@gmail.com>
2018-07-18 03:12:44 -0700

    Add files via upload

---
+++ Leet Code Practice/MaxDepth.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+public class MaxDepth
+{
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        
+        int leftHeight = maxDepth(root.left);    
+        int rightHeight = maxDepth(root.right);
+        
+        return Math.max(leftHeight, rightHeight)+1;
+        
+    }
+}

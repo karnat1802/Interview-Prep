42c26bcac666d93ab6110ef6c29abdee322ef575
karnat1802 <karthiknatarajan2009@gmail.com>
2018-07-19 05:02:36 -0700

    add files via upload

---
+++ Leet Code Practice/PathSum.java
@@ -0,0 +1,18 @@
+
+
+public class PathSum
+{
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null && sum !=0)
+            return false;
+            
+          
+        if(root.left == null && root.right == null && sum - root.val==0 )
+            return true;
+            
+        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum - root.val);
+            
+    }
+    
+    
+}

b7ecb222a4424c0dddadcf11738f524e5b8e6c0f
karnat1802 <karthiknatarajan2009@gmail.com>
2018-07-20 00:51:22 -0700

    Add files via upload

---
+++ Leet Code Practice/Connect.java
@@ -0,0 +1,24 @@
+class TreeLinkNode {
+      int val;
+      TreeLinkNode left, right, next;
+      TreeLinkNode(int x) { val = x; }
+  }
+
+public class Connect
+{
+    public void connect(TreeLinkNode root) {
+        if(root == null)
+            return;
+            
+        if(root.left!= null)
+            root.left.next = root.right;
+            
+        if(root.right != null)
+            root.right  = (root.next != null)? root.next.left : null;
+            
+        connect(root.left);
+        
+        connect(root.right);
+            
+    }
+}

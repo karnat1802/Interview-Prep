2b74492989bf2592c289743876b1a98adfb5029c
Karthik <karthiknatarajan2009@gmail.com>
2018-08-02 21:00:49 -0700

    Add files via upload

---
+++ Leet Code Practice/KthSmallest.java
@@ -0,0 +1,24 @@
+
+
+public class KthSmallest
+{
+    public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        
+        if(k <= count)
+            return kthSmallest(root.left, k);
+        else if(k> count + 1)
+            return kthSmallest(root.right, k - 1 - count);
+            
+        return root.val;    
+    }
+    
+    public int countNodes(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+            
+        return 1 + countNodes(root.left) + countNodes(root.right);    
+    }
+    
+}

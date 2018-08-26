085e7d3c3e2d6d5210e5f5815c1c81a34aba8517
Karthik <karthiknatarajan2009@gmail.com>
2018-08-25 21:19:14 -0700

    add files via upload

---
+++ Leet Code Practice/largestRectArea.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class largestRectArea
+{
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int maxArea = 0;
+        for(int i = 0; i<=heights.length; i++)
+        {
+            int h = (i==heights.length ? 0: heights[i]);
+            if(stack.isEmpty() || h>=heights[stack.peek()])
+                stack.push(i);
+            else{
+                int tp = stack.pop();
+                maxArea = Math.max(maxArea, heights[tp]*(stack.isEmpty()?i:i-1-stack.peek()));
+                i--;
+            }
+        }
+        return maxArea;
+    }
+}

658cf462635f3a3b21b3369c0dc8b765ad62c77d
Karthik Natarajan <karthiknatarajan2009@gmail.com>
2019-01-12 11:49:34 -0800

    A more concise solution

--- Leet Code Practice/MergeTwoLists.java
+++ Leet Code Practice/MergeTwoLists.java
@@ -3,85 +3,43 @@ import java.util.*;
 public class MergeTwoLists
 {
    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode curr = null;
-        ListNode head = null;
-        ListNode n = null;
-        //Deal with edge cases
         if(l1 == null)
             return l2;
-            
-        if (l2 == null)
+        if(l2 == null)
             return l1;
-            
-        if(l1.val<= l2.val)
-        {
-           
-            n= new ListNode(l1.val);
-            curr = n;
-            head = n;
-            l1 = l1.next;
-            // listNodeToString(curr);
-        }
-        
-        else{
-           
-            n= new ListNode(l2.val);
-            curr = n;
-            head = n;
-            l2 = l2.next;
-            // listNodeToString(curr);
-        }
-        
-        while(l1!=null || l2!=null)
-        {
-            if(l1 == null)
-            {
-                
-                curr.next = l2;
-                
-                return head;
-                
-            }
-            
-            else if (l2 == null)
-            {
-                
-                curr.next = l1;
-                
-                return head;
-                
-            }
-            
-            
-            else if(l1!=null && l2!=null && l1.val <= l2.val)
-            {
-                
-                n= new ListNode(l1.val);
-                
-                curr.next = n;
-                curr = curr.next;
-                
-                l1 = l1.next;
-                
-                
-            }
-            
-            else if(l1!=null && l2!=null && l2.val < l1.val)
-            {
-                
-                n= new ListNode(l2.val);
-                
-                curr.next = n;
-                curr = curr.next;
-                
+        ListNode head = null;
+        ListNode curr = null;
+        while(l1 != null || l2 != null){
+            ListNode dummy = null;
+            if (l1 == null){
+                dummy = new ListNode(l2.val);
                 l2 = l2.next;
-               
-                
+            }
+            
+            else if ( l2 == null){
+                dummy = new ListNode(l1.val);
+                l1 = l1.next;
+            }    
+            else if(l1.val <= l2.val)
+            {
+                dummy = new ListNode(l1.val);
+                l1 = l1.next;
+            }       
+            else {
+                dummy = new ListNode(l2.val);
+                l2 = l2.next;
             }
             
             
+            if (head == null){
+                head = dummy;
+                curr = head;
+            }
+            else{
+                curr.next = dummy;
+                curr = curr.next;
+           }
         }
-        
-        return head;
+        return head; 
     }
 }
